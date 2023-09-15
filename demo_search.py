c0e4a2dcdcd38c782e4764f3ce9a17e8ec5b6e4c
mrfakename <me@mrfake.name>
2023-09-15 16:29:46 -0700

    remove custom path in search

--- demo_search.py
+++ demo_search.py
@@ -1,5 +1,5 @@
 from deepsearchkit import DSK
-dsk = DSK(path="sentence-transformers/all-MiniLM-L6-v2")
+dsk = DSK()
 dsk.loadJSON(filename="demo_data.json", column_names=["title", "content"])
 dsk.loadIndex(directory="index_directory")
 while True:
