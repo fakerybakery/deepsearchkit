df51345a0a41d6affbccc9986a8930d6a0107099
mrfakename <me@mrfake.name>
2023-09-15 16:29:19 -0700

    Update error hinting messages

--- deepsearchkit/error_handler.py
+++ deepsearchkit/error_handler.py
@@ -1,12 +1,12 @@
 class DSKUndefinedVariableError(Exception):
-    """Custom DeepSearchKit error"""
+    """Custom DeepSearchKit error: Undefined variable"""
     pass
 class DSKEmptyDataError(Exception):
-    """Custom DeepSearchKit error"""
+    """Custom DeepSearchKit error: Empty dataset"""
     pass
 class DSKUnindexedError(Exception):
-    """Custom DeepSearchKit error"""
+    """Custom DeepSearchKit error: Index hasn't been created yet"""
     pass
 class DSKFeatureNotSupportedError(Exception):
-    """Custom DeepSearchKit error"""
+    """Custom DeepSearchKit error: Unsupported feature/WIP feature"""
     pass
